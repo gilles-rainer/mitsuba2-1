7255acf15be4556761e8631d76994957281c2785
gilles-rainer <gilles.rainer.enst@gmail.com>
2021-04-06 19:52:25 +0200

    made AOVs aliased

--- src/bsdfs/diffuse.cpp
+++ src/bsdfs/diffuse.cpp
@@ -139,9 +139,7 @@ public:
     }
 
     Color3f get_sg_bsdf(const SurfaceInteraction3f &si) const {
-        auto si_copy = si;
-        si_copy.wi   = Vector3f(0.0, 0.0, 1.0);
-        return m_reflectance->eval(si_copy, true) * math::InvPi<Float>;
+        return m_reflectance->eval(si, true) * math::InvPi<Float>;
     }
 
     Float get_alpha(const SurfaceInteraction3f &si) const {
